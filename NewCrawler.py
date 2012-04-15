ae24fc3af0f800bac1fc859b500cef5a8095bce0
madhurimamalla <madhurima2627@gmail.com>
2012-04-15 21:25:52 +0530

    URankAlgoWithaNormalURLLIB

--- NewCrawler.py
+++ NewCrawler.py
@@ -23,125 +23,6 @@ def compute_ranks(graph):
     return ranks
 
 
-
-cache = {
-   'http://udacity.com/cs101x/urank/index.html': """<html>
-<body>
-<h1>Dave's Cooking Algorithms</h1>
-<p>
-Here are my favorite recipies:
-<ul>
-<li> <a href="http://udacity.com/cs101x/urank/hummus.html">Hummus Recipe</a>
-<li> <a href="http://udacity.com/cs101x/urank/arsenic.html">World's Best Hummus</a>
-<li> <a href="http://udacity.com/cs101x/urank/kathleen.html">Kathleen's Hummus Recipe</a>
-</ul>
-
-For more expert opinions, check out the 
-<a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a> 
-and <a href="http://udacity.com/cs101x/urank/zinc.html">Zinc Chef</a>.
-</body>
-</html>
-
-
-
-
-
-
-""", 
-   'http://udacity.com/cs101x/urank/zinc.html': """<html>
-<body>
-<h1>The Zinc Chef</h1>
-<p>
-I learned everything I know from 
-<a href="http://udacity.com/cs101x/urank/nickel.html">the Nickel Chef</a>.
-</p>
-<p>
-For great hummus, try 
-<a href="http://udacity.com/cs101x/urank/arsenic.html">this recipe</a>.
-
-</body>
-</html>
-
-
-
-
-
-
-""", 
-   'http://udacity.com/cs101x/urank/nickel.html': """<html>
-<body>
-<h1>The Nickel Chef</h1>
-<p>
-This is the
-<a href="http://udacity.com/cs101x/urank/kathleen.html">
-best Hummus recipe!
-</a>
-
-</body>
-</html>
-
-
-
-
-
-
-""", 
-   'http://udacity.com/cs101x/urank/kathleen.html': """<html>
-<body>
-<h1>
-Kathleen's Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Open a can of garbonzo beans.
-<li> Crush them in a blender.
-<li> Add 3 tablesppons of tahini sauce.
-<li> Squeeze in one lemon.
-<li> Add salt, pepper, and buttercream frosting to taste.
-</ol>
-
-</body>
-</html>
-
-""", 
-   'http://udacity.com/cs101x/urank/arsenic.html': """<html>
-<body>
-<h1>
-The Arsenic Chef's World Famous Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Kidnap the <a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a>.
-<li> Force her to make hummus for you.
-</ol>
-
-</body>
-</html>
-
-""", 
-   'http://udacity.com/cs101x/urank/hummus.html': """<html>
-<body>
-<h1>
-Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Go to the store and buy a container of hummus.
-<li> Open it.
-</ol>
-
-</body>
-</html>
-
-
-
-
-""", 
-}
-
 def crawl_web(seed): # returns index, graph of inlinks
     tocrawl = [seed]
     crawled = []
@@ -162,12 +43,13 @@ def crawl_web(seed): # returns index, graph of inlinks
             crawled.append(page)
     return index, graph
 
-
 def get_page(url):
-    if url in cache:
-        return cache[url]
-    else:
-        return None
+	try:
+		import urllib
+		return urllib.urlopen(url).read()
+	except:
+		return ""
+
     
 def get_next_target(page):
     start_link = page.find('<a href=')
